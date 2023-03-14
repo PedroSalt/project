3f173e5441e616469b5c14c025a3bca9ad6424dc
salt-dev <salt-dev@salt-devs-Air-8.home>
2023-03-14 15:39:02 +0100

    changed API, added .env system and added the like button

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -1,48 +1,35 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useRef, useState } from "react";
 
 export const Gallery = () => {
-  const [input, setInput] = useState("");
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInput(e.currentTarget.value);
-  };
-
   const [imgUrl, setImgUrl] = useState("");
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    if (!input.length) {
-      return console.log("gotta write something");
-      // maybe {input.length? normal button : disabled btn}
-    }
-
-    // fetch(`https://cataas.com/cat/says/${input}`)
-    //   // .then((response) => response.json())
-    //   .then((data) => {
-    //     setImgUrl(data.url);
-    //     console.log(data.url);
-    //   });
-    const data = await fetch(`https://cataas.com/cat/says/${input}`);
-    setImgUrl(data.url);
-    setInput("");
-    console.log(data);
+    const api_key = process.env.REACT_APP_API_KEY;
+    const url = `https://api.thecatapi.com/v1/images/search?&api_key=${api_key}`;
+    const data = await fetch(url);
+    const parsedData = await data.json();
+    const parsedDataURL = parsedData[0].url;
+    setImgUrl(parsedDataURL);
   };
+
+  const handleLike = () => {
+    const img = image.current! as HTMLImageElement;
+    console.log(img.src);
+  };
+  const image = useRef(null);
 
   return (
     <div>
       <h1>Get a random cat picture</h1>
       <form onSubmit={handleSubmit} action="" method="get">
-        <input
-          type="text"
-          onChange={handleInput}
-          placeholder="What does the cat say?"
-          minLength={1}
-        />
         <p>
           <button type="submit">Cat me</button>
         </p>
         <p>
-          <img src={imgUrl} alt="" />
+          <img src={imgUrl} alt="" ref={image} />
         </p>
       </form>
+      <button onClick={handleLike}>Like</button>
     </div>
   );
 };
